5094093ab445a824815c5f544574f168e649ecf6
Luis Herrera <lherrera@gignosoft.cl>
2016-10-05 09:20:25 -0400

    pequeño avance

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -52,28 +52,53 @@ Route::group(['middleware' => ['web']], function () {
         /*****************************/
 
         /* create */
-        Route::get('/ingresarUsuario',  'Mantenedores\MantenedorDeUsuarios@ingresarUsuario')->name('ingresarUsuario');
-        Route::post('/ingresarUsuario',   'Mantenedores\MantenedorDeUsuarios@accionIngresarUsuario');
+        Route::get('/ingresarUsuario',
+            'Mantenedores\MantenedorDeUsuarios@ingresarUsuario')
+            ->name('ingresarUsuario');
+
+        Route::post('/ingresarUsuario',
+            'Mantenedores\MantenedorDeUsuarios@accionIngresarUsuario')
+            ->name('ingresarUsuario');;
+
 
         /* update */
-        Route::get('actualizarUsuario/{id}',    'Mantenedores\MantenedorDeUsuarios@actualizar')->name('actualizarUsuario');
-        Route::post('actualizarUsuario/{id}',   'Mantenedores\MantenedorDeUsuarios@accionActializarUsuario')->name('actualizarUsuario');
-        Route::get('/restablecerContrasenia/{id}',   'Mantenedores\MantenedorDeUsuarios@restablecerContrasenia')->name('restablecerContrasenia');
+        Route::get('actualizarUsuario/{id}',
+            'Mantenedores\MantenedorDeUsuarios@actualizar')
+            ->name('actualizarUsuario');
+
+        Route::post('actualizarUsuario/{id}',
+            'Mantenedores\MantenedorDeUsuarios@accionActializarUsuario')
+            ->name('actualizarUsuario');
+
+        Route::get('/restablecerContrasenia/{id}',
+            'Mantenedores\MantenedorDeUsuarios@restablecerContrasenia')
+            ->name('restablecerContrasenia');
 
         /* delete */
-        Route::get('eliminarUsuario/{id}', 'Mantenedores\MantenedorDeUsuarios@eliminarUsuario')->name('eliminarUsuario');
+        Route::get('eliminarUsuario/{id}',
+            'Mantenedores\MantenedorDeUsuarios@eliminarUsuario')
+            ->name('eliminarUsuario');
 
         /* list */
-        Route::get('/listarUsuario',    'Mantenedores\MantenedorDeUsuarios@listarTodos')->name('listarUsuario');
-        Route::post('/listarUsuario',   'Mantenedores\MantenedorDeUsuarios@buscarUsuario');
+        Route::get('/listarUsuario',
+            'Mantenedores\MantenedorDeUsuarios@listarTodos')
+            ->name('listarUsuario');
+
+        Route::post('/listarUsuario',
+            'Mantenedores\MantenedorDeUsuarios@buscarUsuario');
 
 
         /* ver */
-        Route::get('/verUsuario/{id}', 'Mantenedores\MantenedorDeUsuarios@verUsuario')->name('verUsuario');
+        Route::get('/verUsuario/{id}',
+            'Mantenedores\MantenedorDeUsuarios@verUsuario')
+            ->name('verUsuario');
 
         // CARGA EL COMBO CIUDAD CON EL ID DEL PAÍS
-        Route::get('/cargaCiudadUsuario/{id}', 'Mantenedores\MantenedorDeUsuarios@cargaCiudadUsuario');
-        Route::post('/cargaCiudadUsuario', 'Mantenedores\MantenedorDeUsuarios@cargaCiudadUsuario');
+        Route::get('/cargaCiudadUsuario/{id}',
+            'Mantenedores\MantenedorDeUsuarios@cargaCiudadUsuario');
+
+        Route::post('/cargaCiudadUsuario',
+            'Mantenedores\MantenedorDeUsuarios@cargaCiudadUsuario');
 
         /**********************************/
         /** MANTENEDOR DE CARGOS        **/
